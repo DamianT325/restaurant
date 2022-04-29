df499749127dfb70eab8c9da6e98d6ef0f25ab11
DamianT325 <dtramowski789@interia.pl>
2022-04-29 23:37:04 +0200

    starting making navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
-
-import './Navbar.css';
+import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
+import images from "../../constants/images";
+import "./Navbar.css";
 
 const Navbar = () => (
-  <div>
-    Navbar
+  <div className="app__navbar">
+    <div className="app__navbar-logo">
+      <img src={images.gericht} alt="logo" className="app__navbar-logo-img" />
+    </div>
+    <ul className="app__navbar-links">
+      <li className="app__navbar-links-link" id="home">
+        Home
+      </li>
+      <li className="app__navbar-links-link" id="Pages">
+        Pages
+      </li>
+      <li className="app__navbar-links-link" id="Contact Us">
+        Contact Us
+      </li>
+      <li className="app__navbar-links-link" id="Blog">
+        Blog
+      </li>
+      <li className="app__navbar-links-link" id="Landing">
+        Landing
+      </li>
+    </ul>
+    <div className="app__navbar-logInLogOut">
+      <p className="app__navbar-logInLogOut-paragraph">Log In / Registration</p>
+      <div></div>
+      <p className="app__navbar-logInLogOut-paragraph"> Book Table</p>
+    </div>
   </div>
 );
 
