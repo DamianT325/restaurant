2e562beeefd5f9fc8f003deeae3ab3e6b5ef098c
DamianT325 <dtramowski789@interia.pl>
2022-05-03 21:07:57 +0200

    started chef section

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
-
-import './Chef.css';
+import React from "react";
+import { images } from "../../constants";
+import { SubHeading } from "../../components";
+import "./Chef.css";
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className="app__chef app__bg app__wraper section__padding">
+    <div className="app__chef_image">
+      <img src={images.chef} alt="chef" />
+    </div>
+    <div className="app__chef_heading">
+      <SubHeading title={"Chef’s Word"} />
+      <h1 className="headtext__cormorant">What we believe in</h1>
+      <p className="p__opensans">
+        auctor sit iaculis in arcu. Vulputate nulla lobortis mauris eget sit.
+        Nulla scelerisque scelerisque congue ac consequat, aliquam molestie
+        lectus eu. Congue iaculis integer curabitur semper sit nunc.
+      </p>
+    </div>
   </div>
 );
 
