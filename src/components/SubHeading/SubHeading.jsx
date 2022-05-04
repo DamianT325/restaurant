209e5bb68c67717a954f56f897cf5620ded31e1c
DamianT325 <dtramowski789@interia.pl>
2022-05-04 10:06:47 +0200

    fixed flexbox on subheading

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { images } from "../../constants";
 const SubHeading = ({ title }) => (
-  <div style={{ marginBottom: "1rem" }} className="p__cormorant">
+  <div
+    style={{
+      marginBottom: "1rem",
+      flexDirection: "column",
+      display: "flex",
+      alignItems: "flex-start",
+    }}
+    className="p__cormorant "
+  >
     {title}
-    <img
-      src={images.spoon}
-      alt="spoon"
-      className="spoon__image"
-      style={{ marginLeft: "1rem" }}
-    />
+
+    <img src={images.spoon} alt="spoon" className="spoon__img" />
   </div>
 );
 
--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -4,18 +4,14 @@ import { SubHeading } from "../../components";
 import "./Chef.css";
 
 const Chef = () => (
-  <div className="app__chef app__bg app__wraper section__padding">
-    <div className="app__chef_image">
-      <img src={images.chef} alt="chef" />
+  <div className="app__chef app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="" />
     </div>
-    <div className="app__chef_heading">
+
+    <div className="app__wrapper_info">
       <SubHeading title={"Chef’s Word"} />
       <h1 className="headtext__cormorant">What we believe in</h1>
-      <p className="p__opensans">
-        auctor sit iaculis in arcu. Vulputate nulla lobortis mauris eget sit.
-        Nulla scelerisque scelerisque congue ac consequat, aliquam molestie
-        lectus eu. Congue iaculis integer curabitur semper sit nunc.
-      </p>
     </div>
   </div>
 );
